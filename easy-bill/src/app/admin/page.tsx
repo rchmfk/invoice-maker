d9d67f33770e90b13e5bd0bf3a840a43215ca10b
RachmaFauziah Krismayanti <rachmafrau@gmail.com>
2025-01-14 15:40:35 +0700

    add heading

--- easy-bill/src/app/admin/page.tsx
+++ easy-bill/src/app/admin/page.tsx
@@ -1,10 +1,59 @@
+import {
+  ChevronRightIcon,
+  PlusIcon,
+} from "@heroicons/react/20/solid";
+import Link from "next/link";
+
 const AdminPage = () => {
-    return (
-      <div>
-        <h1>Dashboard Admin</h1>
-        <p>Welcome to the Admin Dashboard!</p>
-      </div>
-    );
-  };
-  
-  export default AdminPage;
+  return (
+    <>
+      <main>
+        <div className="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
+          <div className="lg:flex lg:items-center lg:justify-between">
+            <div className="min-w-0 flex-1">
+              <h2 className="text-2xl/7 font-bold text-gray-900 sm:truncate sm:text-3xl sm:tracking-tight">
+                Title
+              </h2>
+              <div className="mt-1 flex flex-col sm:mt-0 sm:flex-row sm:flex-wrap sm:space-x-2">
+                <Link href="" className="mt-2 flex items-center text-sm text-gray-500">
+                  Menu
+                </Link>
+                <Link href="" className="mt-2 flex items-center text-sm text-gray-500">
+                  <ChevronRightIcon
+                    aria-hidden="true"
+                    className="mr-2 size-5 shrink-0 text-gray-400"
+                  />
+                  Sub Menu
+                </Link>
+                <Link href="" className="mt-2 flex items-center text-sm text-gray-500">
+                  <ChevronRightIcon
+                    aria-hidden="true"
+                    className="mr-2 size-5 shrink-0 text-gray-400"
+                  />
+                  Sub Menu
+                </Link>
+              </div>
+            </div>
+            <div className="mt-5 flex lg:ml-4 lg:mt-0">
+
+              <span className="sm:ml-3">
+                <button
+                  type="button"
+                  className="inline-flex items-center rounded-md bg-rose-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-rose-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-rose-600"
+                >
+                  <PlusIcon
+                    aria-hidden="true"
+                    className="-ml-0.5 mr-1.5 size-5"
+                  />
+                  Add
+                </button>
+              </span>
+            </div>
+          </div>
+        </div>
+      </main>
+    </>
+  );
+};
+
+export default AdminPage;
