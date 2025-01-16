42290469a62c501219255cac71a0e1388c11b870
FirmanKhoiril <firmankhoiril13@gmail.com>
2025-01-16 18:59:58 +0700

    adding middleware but still figuring out why didnt work, and done admin create invoice view & admin invoice page

---
+++ easy-bill/src/components/Invoice/FormInvoice.tsx
@@ -0,0 +1,349 @@
+"use client";
+import {
+  InvoiceFormValues,
+  invoiceSchema,
+} from "@/typescript/entities/FormInvoice";
+import {
+  ArrowsPointingOutIcon,
+  PlusIcon,
+  TrashIcon,
+} from "@heroicons/react/24/outline";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useState } from "react";
+import { SubmitHandler, useFieldArray, useForm, useWatch } from "react-hook-form";
+import InputInvoice from "./InputInvoice";
+import { calculateTotal, formatNumber } from "@/utils/formating";
+
+const FormInvoice = () => {
+  const [indexPage, setIndexPage] = useState("invoice_data");
+  const {
+    control,
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<InvoiceFormValues>({
+    resolver: zodResolver(invoiceSchema),
+    defaultValues: {
+      invoiceNo: "",
+      date: "",
+      maker: "",
+      currency: "",
+      client: "",
+      dueDate: "",
+      contactPerson: "",
+      terms: "",
+      notes: "",
+      termsAndCondition: "",
+      items: [
+        { item: "", description: "", quantity: 1, discount: "", price: 0 },
+        { item: "", description: "", quantity: 1, discount: "", price: 0 },
+        { item: "", description: "", quantity: 1, discount: "", price: 0 },
+      ],
+    },
+  });
+
+  const { fields, append, remove } = useFieldArray({
+    control,
+    name: "items",
+  });
+
+  const additionalDiscount = useWatch({
+    control,
+    name: "additionalDiscount",
+    defaultValue: "",
+  });
+
+  const shippingCost = useWatch({
+    control,
+    name: "shippingCost",
+    defaultValue: "",
+  });
+
+
+  
+
+  const subTotal = 10000000;
+  const discountTotal = 2000000;
+
+  const total = calculateTotal({
+    subTotal,
+    discountTotal,
+    additionalDiscount: Number(additionalDiscount || 0),
+    shippingCost: Number(shippingCost || 0),
+  });
+
+  const onSubmit: SubmitHandler<InvoiceFormValues> = (data) => {
+    console.log("Invoice Data:", data);
+    setIndexPage("invoice_template");
+  };
+
+  const handleSaveInvoice = () => {
+
+  }
+
+  return (
+    <div className="flex flex-col gap-8">
+      <div className="w-full mt-12">
+        <div className="flex items-center max-w-5xl mx-auto pb-10 justify-center gap-8">
+          <div className="flex flex-col items-center text-center">
+            <div className="flex items-center justify-center w-8 h-8 bg-green-500 text-white rounded-full">
+              1
+            </div>
+            <p className="text-sm text-black mt-2">Invoice Data</p>
+          </div>
+          <div className="h-[2px] bg-gray-200 flex-1"></div>
+          <div className="flex flex-col items-center text-center opacity-50">
+            <div className="flex items-center justify-center w-8 h-8 bg-gray-400 text-white rounded-full">
+              3
+            </div>
+            <p className="text-sm text-gray-500 mt-2">Invoice Template</p>
+          </div>
+        </div>
+      </div>
+
+      {indexPage === "invoice_data" ? (
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <div className="relative mb-10">
+            <div className="h-[2px] bg-gray-200 top-4 -z-10 absolute w-full"></div>
+            <h2 className="mt-8 text-lg font-semibold w-auto bg-white max-w-16 text-black">
+              Invoice
+            </h2>
+          </div>
+          <div className="grid grid-cols-2 gap-8">
+            <InputInvoice
+              id="invoiceNo"
+              label="No"
+              type="text"
+              placeholder="INV/2025/0001"
+              register={register}
+              required={true}
+              error={errors.invoiceNo?.message}
+            />
+            <InputInvoice
+              id="date"
+              label="Date"
+              type="date"
+              placeholder=""
+              register={register}
+              required={true}
+              error={errors.date?.message}
+            />
+            <InputInvoice
+              id="maker"
+              label="Maker"
+              type="text"
+              placeholder="Alice"
+              register={register}
+              required={true}
+              error={errors.maker?.message}
+            />
+            <InputInvoice
+              id="terms"
+              label="Terms"
+              type="text"
+              placeholder="Net 7 Days"
+              register={register}
+              required={true}
+              error={errors.terms?.message}
+            />
+            <InputInvoice
+              id="currency"
+              label="Currency"
+              type="text"
+              placeholder="Dollar (USD)"
+              register={register}
+              required={true}
+              error={errors.currency?.message}
+            />
+            <InputInvoice
+              id="dueDate"
+              label="Due Date"
+              type="date"
+              placeholder=""
+              register={register}
+              required={true}
+              error={errors.dueDate?.message}
+            />
+            <InputInvoice
+              id="client"
+              label="Client"
+              type="text"
+              placeholder="John Wick"
+              register={register}
+              required={true}
+              error={errors.client?.message}
+            />
+            <InputInvoice
+              id="contactPerson"
+              label="Contact Person"
+              type="text"
+              placeholder="John Doe"
+              register={register}
+              required={true}
+              error={errors.contactPerson?.message}
+            />
+            <InputInvoice
+              id="notes"
+              label="Notes"
+              type="textarea"
+              placeholder="Notes"
+              register={register}
+              rows={5}
+              required={true}
+              error={errors.notes?.message}
+            />
+            <InputInvoice
+              id="termsAndCondition"
+              label="Terms & Conditions"
+              type="textarea"
+              placeholder="Terms & Conditions"
+              register={register}
+              rows={5}
+              required={true}
+              error={errors.termsAndCondition?.message}
+            />
+          </div>
+          <div className="w-full flex flex-col gap-8 mt-4">
+            <div className="relative flex justify-between items-center">
+              <div className="h-[2px] bg-gray-200 top-[14px] -z-10 absolute w-full"></div>
+              <h2 className="text-lg font-semibold w-auto bg-white max-w-12 text-black">
+                Items
+              </h2>
+              <button
+                type="button"
+                className="px-2 rounded-lg w-auto flex items-center gap-2 bg-green-600 text-sm py-1 text-white"
+                onClick={() =>
+                  append({
+                    item: "",
+                    description: "",
+                    quantity: 1,
+                    discount: "",
+                    price: 0,
+                  })
+                }
+              >
+                <PlusIcon className="size-5" />
+                Add Item
+              </button>
+            </div>
+            <div>
+              <div className="flex flex-col gap-4 w-full">
+                {fields.map((item, index) => (
+                  <div key={item.id} className="flex items-center gap-4">
+                    <div className="">
+                      <ArrowsPointingOutIcon className="size-5" />
+                    </div>
+                    <InputInvoice
+                      id={`items[${index}].item`}
+                      label="Item"
+                      type="text"
+                      placeholder="Item"
+                      register={register}
+                    />
+                    <InputInvoice
+                      id={`items[${index}].description`}
+                      label="Description"
+                      type="text"
+                      placeholder="Description"
+                      register={register}
+                    />
+                    <InputInvoice
+                      id={`items[${index}].quantity`}
+                      label="Quantity"
+                      type="text"
+                      placeholder="1"
+                      register={register}
+                    />
+                    <InputInvoice
+                      id={`items[${index}].discount`}
+                      label="Discount"
+                      type="text"
+                      placeholder="%"
+                      register={register}
+                    />
+                    <InputInvoice
+                      id={`items[${index}].price`}
+                      label="Price"
+                      type="number"
+                      placeholder="0"
+                      register={register}
+                    />
+                    <button type="button" onClick={() => remove(index)}>
+                      <TrashIcon className="size-5 text-red-500" />
+                    </button>
+                  </div>
+                ))}
+              </div>
+            </div>
+            <div className="w-full flex flex-col gap-2">
+              <div className="relative flex justify-between items-center">
+                <div className="h-[2px] bg-gray-200 top-[14px] -z-10 absolute w-full"></div>
+              </div>
+            </div>
+          </div>
+          <div className="mt-4 flex flex-1 justify-end">
+            <div className="max-w-[300px] mt-6 flex flex-col gap-3 w-full">
+              <p className="flex items-center gap-4 justify-between">
+                <span className="text-sm text-gray-600">Sub Total </span>
+                <span> Rp{formatNumber(10000000)}</span>
+              </p>
+              <p className="flex items-center gap-4 justify-between">
+                <span className="text-sm text-gray-600">Discount Total </span>
+                <span> Rp{formatNumber(2000000)}</span>
+              </p>
+              <div className="flex items-center gap-4 justify-between">
+                <p className="text-sm text-gray-600">Additional Discount</p>
+                <div className="max-w-36">
+                  <InputInvoice
+                    id="additionalDiscount"
+                    label=""
+                    type="text"
+                    placeholder="Rp 000.000,00"
+                    register={register}
+                    error={errors.additionalDiscount?.message}
+                  />
+                </div>
+              </div>
+              <div className="flex items-center gap-4 justify-between">
+                <p className="text-sm text-gray-600">Shipping Cost</p>
+                <div className="max-w-36">
+                  <InputInvoice
+                    id="shippingCost"
+                    label=""
+                    type="text"
+                    placeholder="Rp 000.000,00"
+                    register={register}
+                    error={errors.shippingCost?.message}
+                  />
+                </div>
+              </div>
+              <p className="flex items-center gap-4 justify-between">
+                <span className="text-sm text-gray-600">Total </span>
+                <span className="font-semibold"> Rp{formatNumber(total)}</span>
+              </p>
+            </div>
+          </div>
+          <div className="w-full flex items-center gap-8 flex-row mt-10">
+            <button
+              type="button"
+              onClick={handleSaveInvoice}
+              className="py-2 bg-green-600 hover:bg-green-500 text-white w-full rounded-lg font-semibold"
+            >
+              Save
+            </button>
+            <button
+              type="submit"
+              className="py-2 border border-green-600 hover:border-green-500 text-green-600 hover:text-green-500 w-full rounded-lg font-semibold"
+            >
+              Next
+            </button>
+          </div>
+        </form>
+      ) : (
+        <div className="">Test</div>
+      )}
+    </div>
+  );
+};
+
+export default FormInvoice;
---
+++ easy-bill/src/components/Invoice/InvoiceOption.tsx
@@ -0,0 +1,133 @@
+"use client";
+
+import { useOutsideClick } from "@/hooks/useOutsideClickEvent";
+import { paymentOption, salesPersonOption } from "@/public/DummtData";
+import { ChevronDownIcon, EllipsisVerticalIcon, MagnifyingGlassIcon } from "@heroicons/react/24/outline";
+import { useState } from "react";
+import { useSearchParams, useRouter } from "next/navigation";
+
+type Name = {
+  name: string;
+};
+
+const InvoiceOption = () => {
+  const searchParams = useSearchParams();
+  const router = useRouter();
+  const [toogleSalesPersonOption, setToogleSalesPersonOption] = useState<boolean>(false);
+  const [tooglePaymentOption, setTooglePaymentOption] = useState<boolean>(false);
+  const [salesPerson, setSalesPerson] = useState<Name>({
+    name: searchParams.get("salesPerson") || "All",
+  });
+  const [paymentProcess, setPaymentProcess] = useState<Name>({
+    name: searchParams.get("paymentProcess") || "All",
+  });
+  const [searchInvoice, setSearchInvoice] = useState(searchParams.get("searchInvoice") || "");
+
+  const salesPersonOptionRef = useOutsideClick(() => setToogleSalesPersonOption(false));
+  const paymentOptionRef = useOutsideClick(() => setTooglePaymentOption(false));
+
+  const updateSearchParams = (key: string, value: string) => {
+    const params = new URLSearchParams(searchParams.toString());
+    if (value) {
+      params.set(key, value);
+    } else {
+      params.delete(key);
+    }
+    router.push(`?${params.toString()}`);
+  };
+
+  const handleSalesPersonChange = (name: string) => {
+    setSalesPerson({ name });
+    updateSearchParams("salesPerson", name);
+  };
+
+  const handlePaymentProcessChange = (name: string) => {
+    setPaymentProcess({ name });
+    updateSearchParams("paymentProcess", name);
+  };
+
+  const handleSearchChange = (value: string) => {
+    setSearchInvoice(value);
+    updateSearchParams("client", value);
+  };
+
+  return (
+    <div className="">
+      <div className="flex gap-3 p-8">
+        {/* Sales Person */}
+        <div
+          className="relative border border-black/20 min-w-32 text-center rounded-lg py-4 px-3 hover:border-black/30 transition flex items-center justify-around cursor-pointer"
+          ref={salesPersonOptionRef}
+          onClick={() => setToogleSalesPersonOption((prev) => !prev)}
+        >
+          <p className="px-px absolute text-sm left-5 bg-white -top-2.5 font-semibold text-black/50">
+            Sales Person
+          </p>
+          <p>{salesPerson.name}</p>
+          <ChevronDownIcon
+            className={`size-5 transition ${toogleSalesPersonOption ? "rotate-180" : ""}`}
+          />
+          {toogleSalesPersonOption && (
+            <div className="absolute bg-white top-[57px] overflow-hidden flex flex-col rounded-lg border-black/20 border w-full">
+              {salesPersonOption.map((person: Name) => (
+                <button
+                  key={person.name}
+                  className="text-sm hover:bg-gray-100 py-1"
+                  onClick={() => handleSalesPersonChange(person.name)}
+                >
+                  {person.name}
+                </button>
+              ))}
+            </div>
+          )}
+        </div>
+        {/* Payment Process */}
+        <div
+          className="relative border border-black/20 min-w-32 max-w-44 text-center rounded-lg py-4 px-3 hover:border-black/30 transition flex items-center justify-around cursor-pointer"
+          ref={paymentOptionRef}
+          onClick={() => setTooglePaymentOption((prev) => !prev)}
+        >
+          <p className="px-px absolute text-sm left-4 bg-white -top-2.5 font-semibold text-black/50">
+            Payment
+          </p>
+          <p>{paymentProcess.name}</p>
+          <ChevronDownIcon
+            className={`size-5 transition ${tooglePaymentOption ? "rotate-180" : ""}`}
+          />
+          {tooglePaymentOption && (
+            <div className="absolute bg-white top-[57px] overflow-hidden flex flex-col rounded-lg border-black/20 border w-full">
+              {paymentOption.map((process: Name) => (
+                <button
+                  key={process.name}
+                  className="text-sm hover:bg-gray-100 py-1"
+                  onClick={() => handlePaymentProcessChange(process.name)}
+                >
+                  {process.name}
+                </button>
+              ))}
+            </div>
+          )}
+        </div>
+        {/* Search Invoice */}
+        <div className="w-full relative">
+          <input
+            type="text"
+            className="w-full py-4 border border-black/20 rounded-lg pl-12 pr-4"
+            placeholder="Search..."
+            required
+            value={searchInvoice}
+            onChange={(e) => handleSearchChange(e.target.value)}
+          />
+          <button className="absolute left-4 top-[16px]">
+            <MagnifyingGlassIcon className="size-6 text-black/60" />
+          </button>
+        </div>
+        <button className="ml-2">
+          <EllipsisVerticalIcon className="size-7 hover:text-blue-500" />
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default InvoiceOption;
