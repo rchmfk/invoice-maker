59a9e8389e41b49981213e2d93fc5750781b1214
RachmaFauziah Krismayanti <rachmafrau@gmail.com>
2025-01-20 14:08:42 +0700

    modify page user and add search by email

--- easy-bill/src/app/admin/payment/page.tsx
+++ easy-bill/src/app/admin/payment/page.tsx
@@ -1,9 +1,8 @@
 import PaymentList from "@/components/Payment/PaymentList";
-import { ChevronRightIcon, PlusIcon } from "@heroicons/react/24/outline";
+import { ChevronRightIcon } from "@heroicons/react/24/outline";
 import Link from "next/link";
 
 export default function PaymentAdminPage() {
-
   return (
     <>
       <div className="p-4 sm:ml-64">
@@ -35,8 +34,7 @@ export default function PaymentAdminPage() {
           </div>
           <div className="flex items-center justify-center mb-4 rounded bg-gray-50">
             <div className="flex flex-col rounded-lg bg-white shadow-[0px_5px_10px_0px_rgba(0,0,0,0.05)] gap-2 w-full">
-            <PaymentList />
-              
+              <PaymentList />
             </div>
           </div>
         </div>
