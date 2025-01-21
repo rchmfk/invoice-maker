25b2607d4d083610fa7afc2d439ace75e9ff3fc4
RachmaFauziah Krismayanti <rachmafrau@gmail.com>
2025-01-21 08:41:54 +0700

    modify landing

--- easy-bill/src/app/page.tsx
+++ easy-bill/src/app/page.tsx
@@ -1,5 +1,6 @@
-import Footer from "@/components/Footer";
-import Navbar from "@/components/Navbar";
+import {Footer} from "@/components/Landing/Footer";
+import {Navbar} from "@/components/Landing/Navbar";
+import Image from "next/image";
 
 import { Container } from "@/components/Landing/Container";
 import { Hero } from "@/components/Landing/Hero";
@@ -14,44 +15,48 @@ import { benefitOne, benefitTwo } from "@/components/Landing/data";
 export default function Home() {
   return (
     <>
-      <Navbar />
-      <div className="bg-white">
-        <div className="relative isolate px-6 pt-14 lg:px-8">
-          <div className="mx-auto max-w-2xl py-32 sm:py-48 lg:py-56">
-            <div className="hidden sm:mb-8 sm:flex sm:justify-center">
-              <div className="relative rounded-full px-3 py-1 text-sm/6 text-gray-600 ring-1 ring-gray-900/10 hover:ring-gray-900/20">
-                Announcing our next round of funding.{" "}
-                <a href="#" className="font-semibold text-rose-600">
-                  <span aria-hidden="true" className="absolute inset-0" />
-                  Read more <span aria-hidden="true">&rarr;</span>
-                </a>
-              </div>
-            </div>
-            <div className="text-center">
-              <h1 className="text-balance text-5xl font-semibold tracking-tight text-gray-900 sm:text-7xl">
-                Simplify Your Business with Effortless Invoice Creation
-              </h1>
-              <p className="mt-8 text-pretty text-lg font-medium text-gray-500 sm:text-xl/8">
-                Simplify Your Business is an easy-to-use platform designed to
-                make invoice creation quick and hassle-free. With its
-                user-friendly interface and smart automation, you can create
-                professional invoices in seconds.
-              </p>
-              <div className="mt-10 flex items-center justify-center gap-x-6">
-                <a
-                  href="#"
-                  className="rounded-md bg-rose-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-rose-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-rose-600"
-                >
-                  Get started
-                </a>
-                <a href="#" className="text-sm/6 font-semibold text-gray-900">
-                  Learn more <span aria-hidden="true">→</span>
-                </a>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
+      <Container>
+        <Navbar/>
+        <Hero />
+        <SectionTitle
+          preTitle="Features"
+          title=" Our Features You Can Get"
+        >
+          Whether you're running a retail operation, offering services, or managing projects, EasyBill helps streamline your invoicing process and keeps your business running smoothly.
+        </SectionTitle>
+
+        <Benefits data={benefitOne} />
+        <Benefits imgPos="right" data={benefitTwo} />
+
+        <SectionTitle
+          preTitle="Watch a video"
+          title="Learn how to fullfil your needs"
+        >
+          This section is to highlight a promo or demo video of your product.
+          Analysts says a landing page with video has 3% more conversion rate.
+          So, don&apos;t forget to add one. Just like this.
+        </SectionTitle>
+
+        <Video videoId="fZ0D0cnR88E" />
+
+        <SectionTitle
+          preTitle="Testimonials"
+          title="Here's what our customers said"
+        >
+          Testimonials is a great way to increase the brand trust and awareness.
+          Use this section to highlight your popular customers.
+        </SectionTitle>
+
+        <Testimonials />
+
+        <SectionTitle preTitle="FAQ" title="Frequently Asked Questions">
+          Answer your customers possible questions here, it will increase the
+          conversion rate as well as support or chat requests.
+        </SectionTitle>
+
+        <Faq />
+        <Cta />
+      </Container>
       <Footer />
     </>
   );
