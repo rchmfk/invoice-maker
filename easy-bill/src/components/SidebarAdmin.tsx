7066d3d59a13a2ec429044438971fafb21432a8e
Rachma Fauziah <rachma@Rachmas-MacBook-Air.local>
2025-01-24 17:47:58 +0700

    fix bug

--- easy-bill/src/components/SidebarAdmin.tsx
+++ easy-bill/src/components/SidebarAdmin.tsx
@@ -117,6 +117,8 @@ const SidebarAdmin = () => {
                       className="w-8 h-8 rounded-full"
                       src="https://flowbite.com/docs/images/people/profile-picture-5.jpg"
                       alt="user photo"
+                      width={100}
+                      height={100}
                     />
                   </button>
                 </div>
--- easy-bill/src/components/SidebarClient.tsx
+++ easy-bill/src/components/SidebarClient.tsx
@@ -115,6 +115,8 @@ const SidebarClient = () => {
                       className="w-8 h-8 rounded-full"
                       src="https://flowbite.com/docs/images/people/profile-picture-5.jpg"
                       alt="user photo"
+                      width={100}
+                      height={100}
                     />
                   </button>
                 </div>
